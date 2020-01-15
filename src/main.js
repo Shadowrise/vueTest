e30a34c0e9c55b03dc28e31e39db45946e41ae6f
Viacheslav Emtcov <viacheslav.emtkov@datium.fi>
2020-01-15 16:55:30 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue';
+
+// components
+import ElementUI from 'element-ui';
+
+// css
+import 'element-ui/lib/theme-chalk/index.css';
+
+// vue
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app');
